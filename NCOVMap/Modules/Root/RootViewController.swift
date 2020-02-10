69b24753d7ac67e553146805ed4fe41a8f8d2ebd
Dmitriy <ToropkinD@gmail.com>
2020-02-10 12:08:21 +0300

    rootcontroller

--- NCOVMap/Modules/Root/RootViewController.swift
+++ NCOVMap/Modules/Root/RootViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class RootViewController: UIViewController {
     
     var resolver: DIResolver
+    var current: UIViewController!
     
     init(resolver: DIResolver) {
         self.resolver = resolver
@@ -19,5 +20,19 @@ class RootViewController: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.showMapScreen()
+    }
+    
+    private func showMapScreen() {
+        let new = self.resolver.presentMapViewController()
+        self.addChild(new)
+        self.view.addSubview(new.view)
+        new.didMove(toParent: self)
+        
+        self.current = new
     }
 }
