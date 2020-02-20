6de5ead491ad1092fad4cbf156b0478310774467
Dmitriy <ToropkinD@gmail.com>
2020-02-20 16:48:30 +0300

    slider

--- NCOVMap/Modules/Donate Section/Donate/View/DonateViewController.swift
+++ NCOVMap/Modules/Donate Section/Donate/View/DonateViewController.swift
@@ -11,30 +11,30 @@ import SnapKit
 import DevHelper
 
 class DonateViewController: BaseViewController {
-
+    
     var presenter: DonatePresenterProtocol!
-
+    
     private var lbl_counter: UILabel = UILabel.makeLabel(size: 58, weight: .bold, color: R.color.appMarineBlue().unwrapped())
     private var donateSlider = UISlider()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.createUI()
-
+        
     }
-
+    
     private func createUI() {
-
+        
         self.view.backgroundColor = R.color.appDark()
         self.navigationController?.setNavigationBarHidden(true, animated: false)
-
+        
         self.customNavigationTitle(title: R.string.localizable.donate_title())
-
+        
         let lightContainer = UIView()
         lightContainer.backgroundColor = .white
-
+        
         let bearHead = UIImageView(image: R.image.bearHead())
-
+        
         self.view.addSubview(bearHead)
         self.view.addSubview(lightContainer)
         lightContainer.snp.makeConstraints {
@@ -43,38 +43,38 @@ class DonateViewController: BaseViewController {
             $0.top.equalTo(self.view.snp.centerY).offset(-100)
         }
         lightContainer.cornerRadius(10)
-
+        
         bearHead.snp.makeConstraints {
             $0.bottom.equalTo(lightContainer.snp.top).offset(36)
             $0.width.equalTo(166)
             $0.height.equalTo(149)
             $0.centerX.equalToSuperview()
         }
-
+        
         let bearHands = UIImageView(image: R.image.bearHands())
         self.view.addSubview(bearHands)
-
+        
         bearHands.snp.makeConstraints {
             $0.centerY.equalTo(lightContainer.snp.top)
             $0.centerX.equalToSuperview()
             $0.width.equalTo(250)
             $0.height.equalTo(27)
         }
-
+        
         let description_lbl = UILabel.makeLabel(size: 10, weight: .regular, color: R.color.appMarineBlue().unwrapped())
         lightContainer.addSubview(description_lbl)
         description_lbl.snp.makeConstraints {
             $0.top.equalToSuperview().offset(65)
             $0.left.right.equalToSuperview()
         }
-
+        
         description_lbl.numberOfLines = 0
-
+        
         description_lbl.text = R.string.localizable.donate_description()
         description_lbl.textAlignment = .center
-
+        
         let donate_btn = self.createDonateButton()
-
+        
         lightContainer.addSubview(donate_btn)
         donate_btn.snp.makeConstraints {
             $0.bottom.equalToSuperview().offset(-42)
@@ -92,9 +92,11 @@ class DonateViewController: BaseViewController {
         self.lbl_counter.text = "1$"
         
         self.donateSlider.tintColor = R.color.appRed()
-        self.donateSlider.maximumValue = 50
+        self.donateSlider.maximumValue = 100
         self.donateSlider.minimumValue = 1
         self.donateSlider.addTarget(self, action: #selector(self.changeValue(_:)), for: .valueChanged)
+        self.donateSlider.addTarget(self, action: #selector(self.changeDidEnd(_:)), for: .touchUpInside)
+        self.donateSlider.addTarget(self, action: #selector(self.changeDidEnd(_:)), for: .touchUpOutside)
         self.view.addSubview(self.donateSlider)
         self.donateSlider.snp.makeConstraints { (make) in
             make.top.equalTo(self.lbl_counter.snp.bottom).offset(50)
@@ -108,29 +110,60 @@ extension DonateViewController: DonateViewProtocol { }
 
 // MARK: - Private
 extension DonateViewController {
-
+    
     func createDonateButton() -> UIButton {
-
+        
         let button = UIButton()
         button.backgroundColor = R.color.appRed().unwrapped()
         button.setTitleColor(.white, for: .normal)
         button.titleLabel?.lineBreakMode = .byTruncatingTail
         button.contentHorizontalAlignment = .center
-
+        
         button.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .regular)
         button.layer.cornerRadius = 10
         button.setTitle(R.string.localizable.donate_donnateButton_title(), for: .normal)
-
+        
         button.snp.makeConstraints { (make) in
             make.height.equalTo(44)
             make.width.equalTo(140).priority(700)
         }
-
+        
         return button
     }
     
     @objc private func changeValue(_ slider: UISlider) {
+        
         let intValue = Int(slider.value)
-        self.lbl_counter.text = "\(intValue)$"
+        //0.99, 5, 10, 25, 50, 100
+        if (1...17).contains(intValue) {
+            self.lbl_counter.text = "0.99$"
+        } else if (17...34).contains(intValue) {
+            self.lbl_counter.text = "5$"
+        } else if (34...51).contains(intValue) {
+            self.lbl_counter.text = "10$"
+        } else if (51...68).contains(intValue) {
+            self.lbl_counter.text = "25$"
+        } else if (68...85).contains(intValue) {
+            self.lbl_counter.text = "50$"
+        } else if (85...100).contains(intValue) {
+            self.lbl_counter.text = "100$"
+        }
+    }
+    
+    @objc private func changeDidEnd(_ slider: UISlider) {
+        let intValue = Int(slider.value)
+        if (1...17).contains(intValue) {
+            self.donateSlider.setValue(Float(0), animated: true)
+        } else if (17...34).contains(intValue) {
+            self.donateSlider.setValue(Float(25), animated: true)
+        } else if (34...51).contains(intValue) {
+            self.donateSlider.setValue(Float(42), animated: true)
+        } else if (51...68).contains(intValue) {
+            self.donateSlider.setValue(Float(59), animated: true)
+        } else if (68...85).contains(intValue) {
+            self.donateSlider.setValue(Float(76), animated: true)
+        } else if (85...100).contains(intValue) {
+            self.donateSlider.setValue(Float(100), animated: true)
+        }
     }
 }
